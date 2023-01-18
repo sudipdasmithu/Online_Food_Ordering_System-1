41095baa4d00235be8425e5e00ea5a462819a154
LENOVO <LENOVO@admin>
2023-01-18 17:52:47 +0530

    please check my testscript

---
+++ src/test/java/projectPractice/userclass.java
@@ -0,0 +1,13 @@
+package projectPractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class userclass {
+	@Test
+	public void createStudent() {
+		Reporter.log("create student",true);
+	}
+
+}
