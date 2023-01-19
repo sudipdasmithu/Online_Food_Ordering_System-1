383e30f54627da08d62aa99836256efff636aa7c
Sudip <113698188+sudipdasmithu@users.noreply.github.com>
2023-01-19 12:51:07 +0530

    Update Nagbaba.java

--- src/test/java/UserModule/Nagbaba.java
+++ src/test/java/UserModule/Nagbaba.java
@@ -7,5 +7,8 @@ public class Nagbaba {
 @Test
 public void babaji_Test() {
 	Reporter.log("baba ka swaghat new user mea", true);
+	WebDriver driver= new Chromedriver();
+	driver.get("https://localhost:8888");
+	driver.findelement(by.xpath("//a[text='button']")).click();
 }
 }
